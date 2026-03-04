f559d85377fe49bd1b901c2d518ee278045ebaaf
Phathu <maddangerchris@gmail.com>
2026-03-04 20:40:16 +0200

    used navlink instead of link to show active state in header section and added favicons for each page

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,44 +1,45 @@
-import Header from '../components/Header';
-import './TrackingPage.css';
+import Header from "../components/Header";
+import "./TrackingPage.css";
 
 function TrackingPage() {
   return (
     <>
-      
-     <title>Track Order</title>
+      <link rel="icon" type="image/svg+xml" href="/orders-favicon.png" />
+
+      <title>Track Order</title>
 
       <Header />
 
-    <div className="tracking-page">
-      <div className="order-tracking">
-        <a className="back-to-orders-link link-primary" href="/orders">
-          View all orders
-        </a>
+      <div className="tracking-page">
+        <div className="order-tracking">
+          <a className="back-to-orders-link link-primary" href="/orders">
+            View all orders
+          </a>
 
-        <div className="delivery-date">Arriving on Monday, June 13</div>
+          <div className="delivery-date">Arriving on Monday, June 13</div>
 
-        <div className="product-info">
-          Black and Gray Athletic Cotton Socks - 6 Pairs
-        </div>
+          <div className="product-info">
+            Black and Gray Athletic Cotton Socks - 6 Pairs
+          </div>
 
-        <div className="product-info">Quantity: 1</div>
+          <div className="product-info">Quantity: 1</div>
 
-        <img
-          className="product-image"
-          src="images/products/athletic-cotton-socks-6-pairs.jpg"
-        />
+          <img
+            className="product-image"
+            src="images/products/athletic-cotton-socks-6-pairs.jpg"
+          />
 
-        <div className="progress-labels-container">
-          <div className="progress-label">Preparing</div>
-          <div className="progress-label current-status">Shipped</div>
-          <div className="progress-label">Delivered</div>
-        </div>
+          <div className="progress-labels-container">
+            <div className="progress-label">Preparing</div>
+            <div className="progress-label current-status">Shipped</div>
+            <div className="progress-label">Delivered</div>
+          </div>
 
-        <div className="progress-bar-container">
-          <div className="progress-bar"></div>
+          <div className="progress-bar-container">
+            <div className="progress-bar"></div>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
