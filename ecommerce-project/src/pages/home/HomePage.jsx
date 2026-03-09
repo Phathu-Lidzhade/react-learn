472d22f19b184f5146a83b938c83923ecfb86dd4
Phathu <maddangerchris@gmail.com>
2026-03-09 16:18:20 +0200

    create add to cart feature

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -5,7 +5,7 @@ import Checkmark from "../../assets/images/icons/checkmark.png";
 import ProductsGrid from "./ProductsGrid";
 import "./HomePage.css";
 
-function HomePage({ cart }) {
+function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -24,7 +24,7 @@ function HomePage({ cart }) {
       <Header cart={cart} />
 
       <div className="home-page">
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
