7676f0be4f6721d4e4afa9b3831c952d35a2e0f2
Phathu <maddangerchris@gmail.com>
2026-03-11 15:45:00 +0200

    lasson 8 part 2: add update feature with key down function

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -3,19 +3,37 @@ import axios from "axios";
 import { useState } from "react";
 
 function CartItemDetails({ cartItem, loadCart }) {
-  const [ quantityUpdated, setQuantityUpdated ] = useState(false);
+  const [ quantityUpdating, setQuantityUpdating ] = useState(false);
+  const [ quantity, setQuantity ] = useState(cartItem.quantity);
 
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
   };
 
-  const updatePressed = () => {
-    // Switch between true and false for quantityUpdated.
-    if (quantityUpdated) {
-      setQuantityUpdated(false);
+  const updatePressed = async () => {
+    // Switch between true and false for quantityUpdating.
+    if (quantityUpdating) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity: Number(quantity),
+      });
+      await loadCart();
+      setQuantityUpdating(false);
     } else {
-      setQuantityUpdated(true);
+      setQuantityUpdating(true);
+    }
+  };
+
+  const getQuantity = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const updateOnKeyDown = (event) => {
+    if (event.key === "Enter") {
+      updatePressed();
+    } else if (event.key === "Escape") {
+      setQuantity(cartItem.quantity);
+      setQuantityUpdating(false);
     }
   };
 
@@ -30,8 +48,13 @@ function CartItemDetails({ cartItem, loadCart }) {
         <div className="product-quantity">
           <span>
             Quantity:{" "}
+
             <input type="text" className="update-quantity"
-            style={{opacity:quantityUpdated ? 1 : 0}} />
+            style={{opacity:quantityUpdating ? 1 : 0}}
+            value={quantity}
+            onChange={getQuantity}
+            onKeyDown={updateOnKeyDown} />
+
             <span className="quantity-label">{cartItem.quantity}</span>
           </span>
           <span className="update-quantity-link link-primary"
