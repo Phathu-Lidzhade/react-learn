7829d77a70e7fa6018d8f3a87dff67103eece5e7
Phathu <maddangerchris@gmail.com>
2026-03-30 09:48:44 +0200

    typescript basics

---
+++ typescript-basics/sandbox.js
@@ -0,0 +1,8 @@
+"use strict";
+const character = 'Mario';
+console.log(character);
+const inputs = document.querySelectorAll('inputs');
+console.log(inputs);
+inputs.forEach(input => {
+    console.log(input);
+});
